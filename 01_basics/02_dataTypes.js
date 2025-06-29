c3802acb69aace2c97ec149daf03807ccb1f8208
Pawan Sahu <96007722+PawanSahu1@users.noreply.github.com>
2025-06-29 06:27:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser 
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Pawan")
+
+let name = "Pawan Sahu"
+let age = 25
+let isLoggedIn = false
+
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => false/true
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof age)
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined 
