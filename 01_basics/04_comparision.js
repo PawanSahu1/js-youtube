35de5d38007585aaecb25deb4e8c49886913d617
Pawan Sahu <96007722+PawanSahu1@users.noreply.github.com>
2025-06-29 13:46:06 +0530

    Add files via upload

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(3>2)
+// console.log(3<2)
+// console.log(23<=23)
+// console.log(33>=52)
+
+
+// console.log("2">1)
+// console.log("02">1)
+
+
+console.log(null>0)
+console.log(null==0)
+//the reason is that an equality check == and comparisions > < >= <= work differently
+//Comparision converts null to a number, treating it as 0.
+console.log(null>=0)
+console.log("******************************");
+
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+
+console.log("2"===2)
